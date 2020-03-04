1abfe5acbfc8b90da983a92563c67926dd65787c
keiki <50743267+666rejections@users.noreply.github.com>
2020-03-04 14:30:35 +0700

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,10 +1,10 @@
 Locales['en'] = {
-	['press_take_bitcoin'] = 'Press ~INPUT_CONTEXT~ to catch Bitcoin',
-	['press_sell_bitcoin'] = 'Press ~INPUT_CONTEXT~ to sell the BitCoin',
-	['inventory_full'] = 'You can not get more Bicoin because your ~r~Iventory~s~',
-	['take_bitcoin'] = '~y~A Minerar Bitcoin~s~...',
-	['you_dont_have_bitcoin2'] = 'You Do not Have More ~r~Bicoin~s~',
-	['you_dont_have_bitcoin'] = 'You Do not Have More ~r~Bitcoin~s~',
-	['bitcoin_sell'] = 'Sell ~y~x1~s~ ~y~Bitcoin~s~',
-	['bitcoin_selltext'] = '~g~For sale Bitcoin~s~...',
+	['press_take_bitcoin'] = 'press ~INPUT_CONTEXT~ to catch Bitcoin',
+	['press_sell_bitcoin'] = 'press ~INPUT_CONTEXT~ to sell the BitCoin',
+	['inventory_full'] = 'you can not get more Bitcoin because your ~r~inventory~s~',
+	['take_bitcoin'] = '~y~a minerar Bitcoin~s~...',
+	['you_dont_have_bitcoin2'] = 'you do not have more ~r~Bitcoin~s~',
+	['you_dont_have_bitcoin'] = 'you do not have more ~r~Bitcoin~s~',
+	['bitcoin_sell'] = 'sell ~y~x1~s~ ~y~Bitcoin~s~',
+	['bitcoin_selltext'] = '~g~for sale Bitcoin~s~...',
 }
